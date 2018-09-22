a67ce3fd54398c695717119509aca352f7a3da21
VrnKpr <varkap@users.noreply.github.com>
2018-09-22 13:31:32 -0400

    Revert "Merge branch 'master' of https://github.com/varkap/JustKeepGoin-"
    This reverts commit d696d84d8848cd0d5f847743fad9a5e715e98581, reversing
    changes made to 676ea110aebacbdc4be72912d17c04e8967bbc43.

--- SIGGDGAME-master/Assets/Scripts/PlayerMovement.cs
+++ SIGGDGAME-master/Assets/Scripts/PlayerMovement.cs
@@ -31,7 +31,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void increaseMovementSpeed()
     {
-        maxHorizontalVelocity += interval;
         movementSpeed += interval;
     }
 
