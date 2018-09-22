0adbc3ebbcb4b360e310097a9536caf348b5573a
VrnKpr <varkap@users.noreply.github.com>
2018-09-21 22:25:13 -0400

    The Scenes

---
+++ SIGGDGAME-master/Assets/Scripts/ChangeScene1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene1 : MonoBehaviour
+{
+    string SceneName;
+    void OnClick()
+    {
+        SceneManager.LoadScene("TrainingLevel", LoadSceneMode.Additive);
+    }
+}
---
+++ SIGGDGAME-master/README.md
@@ -0,0 +1,8 @@
+# SIGGDGAME
+Our game for SIGGD Game Jam 2018
+
+2D side scroller, set in wilderness
+
+stealth - prioritize platforming and enemy avoidance, unable to directly defeat enemies
+
+avoiding stuff like natural obstacles and bears and everything
